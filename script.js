ca421a51ab5e1eb9b64b8d9e5e8528968d43972a
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-06 17:43:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // your code here
+let textelement=document.getElementById(url);
+document.getElementById("button").addEventListener("click",handleSubmit)
+function handleSubmit(e) {
+	e.preventDefault();
+	let initialUrl=document.getElementById("form").action
+	let name=document.getElementById("name").value;
+	let year=document.getElementById("year").value;
+	if(name && year) initialUrl+='?name='+name+'&year='+year;
+	else if(name)initialUrl+='?name='+name;
+	else initialUrl+='?year='+year;
+	textelement.innerHTML=initialUrl;
+}
