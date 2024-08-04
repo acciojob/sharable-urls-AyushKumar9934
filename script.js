95c5f204b828291a0417687cce5f3cd8e47ecc29
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-08-04 20:29:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // your code here
-// let textelement=document.getElementById(url);
+ let textelement=document.getElementById(url);
 document.getElementById("button").addEventListener("click",handleSubmit)
 function handleSubmit(e) {
-	// e.preventDefault();
-	// let initialUrl=document.getElementById("form").action
-	// let name=document.getElementById("name").value;
-	// let year=document.getElementById("year").value;
-	// if(name && year) initialUrl+='?name='+name+'&year='+year;
-	// else if(name)initialUrl+='?name='+name;
-	// else initialUrl+='?year='+year;
-	// textelement.innerHTML=initialUrl;
+	e.preventDefault();
+	let initialUrl=document.getElementById("form").action
+	let name=document.getElementById("name").value;
+	let year=document.getElementById("year").value;
+	if(name && year) initialUrl+='?name='+name+'&year='+year;
+	else if(name)initialUrl+='?name='+name;
+	else initialUrl+='?year='+year;
+	textelement.innerHTML=initialUrl;
 
-	const myUrl2 = new URL(window.location.toLocaleString()).searchParams;
+// 	const myUrl2 = new URL(window.location.toLocaleString()).searchParams;
 
-const name=myUrl2.get('name')
-const year=myUrl2.get('year');
-	document.getElementById('name').value=name;
-	document.getElementById('year').value=year;
+// const name=myUrl2.get('name')
+// const year=myUrl2.get('year');
+// 	document.getElementById('name').value=name;
+// 	document.getElementById('year').value=year;
 }
